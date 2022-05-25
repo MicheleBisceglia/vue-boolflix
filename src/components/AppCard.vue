c1bd19f80a8c2ecbb18a269457ef3f69fe5cf336
Michele Bisceglia <mikael_bisceglia@libero.it>
2022-05-25 22:24:23 +0200

    add img + stars in cards

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,12 +1,21 @@
 <template>
     <div class="container">
       <div class="ms_container">
+        <img class="card_bg pt-3" 
+          v-if="movie.backdrop_path != null "
+          :src='"https://image.tmdb.org/t/p/w200/" + movie.backdrop_path' alt="">
+        <img class="card_bg pt-3"
+          v-else src="https://image.tmdb.org/t/p/w342/wwemzKWzjKYJFfCeiB57q3r4Bcm.png " alt="netflix">
         <h2>Titolo: {{ movie.title ? movie.title : movie.name }}</h2>
         <h3>Titolo Originale: {{ movie.original_title ? movie.original_title : movie.original_name}} </h3>
         <span>Lingua: </span>
-        <img v-if="flags.includes(movie.original_language)" :src='"../assets/img/flag-" + movie.original_language +".png"' alt="lenguage">
-        <span class="language bg-primary" v-else>{{ movie.original_language}}</span>
-        <div>Valutazione: {{ movie.vote_average }}</div>
+        <img class="flag_circle" 
+          v-if="flags.includes(movie.original_language)" :src='"../assets/img/flag-" + movie.original_language +".png"' alt="lenguage">
+        <span class="language bg-primary" 
+          v-else>{{ movie.original_language}}</span>
+        <div>
+          <i v-for="n in 5" :key="n" class="fa-star stars_color" :class="n <= stars ? 'fas' : 'd-none '"></i>
+        </div>
       </div>
     </div>
 </template>
@@ -22,26 +31,32 @@
            flags: ["it", "en", "fr", "de"],
        }
    },
+   computed: {
+       stars() {
+           return Math.ceil(this.movie.vote_average / 2);
+       }
+       
+   }
    }
 </script>
 
 <style lang="scss" scoped>
+@import '~@fortawesome/fontawesome-free/css/all.min.css';
 .ms_container {
     background-color: white;
     border: solid 1px black;
-    text-align: left;
-    min-height: 200px;
-    padding-left: 15%;
+    text-align: center;
+    min-height: 260px;
     font-size: .8rem;
 }
 h2 {
-    padding-top: 30%;
+    padding-top: 10%;
     font-size: .8rem;
 }
 h3 {
     font-size: .8rem
     }
-img {
+.flag_circle {
     height: 15px;
     width: 15px;
     border-radius: 50%;
@@ -54,6 +69,13 @@ img {
     text-transform: uppercase;
     font-size: 70%;
 }
-    
+.card_bg {
+    object-fit: contain;
+    width: 90%;
+    height: 100px;
+}
+.stars_color {
+    color: #f8cf19;
+}    
 
 </style>
